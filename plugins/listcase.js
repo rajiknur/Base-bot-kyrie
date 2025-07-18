d53c69aa4255774781431a93b92e4d12e327e327
rajiknur <151017317+rajiknur@users.noreply.github.com>
2025-07-19 03:25:46 +0800

    Create listcase.js

---
+++ plugins/listcase.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+
+let handler = async (m, { conn, isCreator, reply }) => {
+  if (!isCreator) return reply(mess.creator)
+
+  let files = fs.readdirSync("./plugins/").filter(file => file.endsWith(".js"))
+  if (!files.length) return reply("Tidak ada file plugin ditemukan di folder plugins.")
+
+  let list = files.map((file, i) => `📁 ${i + 1}. ${file}`).join("\n")
+  return reply(`📂 Daftar Plugin:\n\n${list}`)
+}
+
+handler.command = ["listcase", "listplugin", "cekplugins"]
+module.exports = handler
