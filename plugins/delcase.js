5630ffa1499336dfba15a2fc3cbd305e6d947681
rajiknur <151017317+rajiknur@users.noreply.github.com>
2025-07-19 03:25:20 +0800

    Create delcase.js

---
+++ plugins/delcase.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+
+let handler = async (m, { conn, isCreator, text, reply, example }) => {
+  if (!isCreator) return reply(mess.creator)
+  if (!text) return reply(example("namafile.js"))
+  if (!text.endsWith(".js")) return reply("Nama file harus berformat .js")
+
+  let filePath = "./plugins/" + text
+  if (!fs.existsSync(filePath)) return reply("Plugin tidak ditemukan di folder plugins!")
+
+  fs.unlinkSync(filePath)
+  return reply(`Berhasil menghapus file plugins *${text}*`)
+}
+
+handler.command = ["delcase", "deleteplugin", "hapusplugin"]
+module.exports = handler
